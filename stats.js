87454041b7a53ec21b04ee1dc6017735bfd94dc2
Rex763857 <memmy7508@gmail.com>
2024-10-04 21:34:36 +0100

    first commit

--- stats.js
+++ stats.js
@@ -1,6 +1,9 @@
 const people = asserts
 let successful = excel
+let actor = actress
 let rich = wealthy
 let heaven = earth
 const time = controls 
 let change = Comtemporary
+let association = society
+
--- status.js
+++ status.js
@@ -1,4 +1,7 @@
 const myName = " Rex_Bug"
+const heart = "love"
 let myage = 56
 let nationality = "japan"
 let hobbies = ["reading, coding, palyimg"]
+let nigeria = "country"
+let villa = "cities"
